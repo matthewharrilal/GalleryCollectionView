3a260c20ac3e620e67640c7178c61bf5f605a731
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-08-10 00:53:26 -0400

    Didn't need the blended color method ... achieved the wrong effect

--- GalleryView/Helpers/UIColor+Extension.swift
+++ GalleryView/Helpers/UIColor+Extension.swift
@@ -29,31 +29,4 @@ extension UIColor {
         
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
-    
-    static func blendColor(from startColor: UIColor, to endColor: UIColor, percentage: CGFloat) -> UIColor {
-        // Ensure percentage is between 0 and 1
-        let clampedPercentage = max(0, min(percentage, 1))
-        
-        // Get the RGBA components of the colors
-        var startRed: CGFloat = 0
-        var startGreen: CGFloat = 0
-        var startBlue: CGFloat = 0
-        var startAlpha: CGFloat = 0
-        startColor.getRed(&startRed, green: &startGreen, blue: &startBlue, alpha: &startAlpha)
-        
-        var endRed: CGFloat = 0
-        var endGreen: CGFloat = 0
-        var endBlue: CGFloat = 0
-        var endAlpha: CGFloat = 0
-        endColor.getRed(&endRed, green: &endGreen, blue: &endBlue, alpha: &endAlpha)
-        
-        // Interpolate between the colors
-        let blendedRed = startRed + (endRed - startRed) * clampedPercentage
-        let blendedGreen = startGreen + (endGreen - startGreen) * clampedPercentage
-        let blendedBlue = startBlue + (endBlue - startBlue) * clampedPercentage
-        let blendedAlpha = startAlpha + (endAlpha - startAlpha) * clampedPercentage
-        
-        // Return the blended color
-        return UIColor(red: blendedRed, green: blendedGreen, blue: blendedBlue, alpha: blendedAlpha)
-    }
 }
